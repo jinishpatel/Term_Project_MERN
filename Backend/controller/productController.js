37386a42c9aa451298893429ae4296a9005f07e9
Jinish Patel <jinish5597@gmail.com>
2023-07-23 18:54:56 -0400

    mongo

--- Backend/controller/productController.js
+++ Backend/controller/productController.js
@@ -1,4 +1,15 @@
-exports.getProducts = (req, res) =>
-{
-    res.status(200).json({message: "This is the getProducts route"})
+const Product = require("../models/productModel");
+const mongoose = require("mongoose");
+// Create new product => /api/v1/admin/product/new
+
+exports.createProduct = async (req, res, next) => {
+  const product = await Product.create(req.body);
+
+  res.status(201).json({
+    success: true,
+    product,
+  });
+};
+exports.getProducts = (req, res) => {
+  res.status(200).json({ message: "This is the getProducts route" });
 };
